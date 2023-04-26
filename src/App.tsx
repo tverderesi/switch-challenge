759039dad23c8ebecd70155e1e17c94caba4c359
tverderesi <tverderesi@gmail.com>
2023-04-26 13:34:13 -0300

    updated with final code

--- src/App.tsx
+++ src/App.tsx
@@ -4,39 +4,64 @@ import { SwitchView } from "./components/switchView";
 
 function App(): JSX.Element {
   const [darkMode, setDarkMode] = useState<Boolean>(false);
-  const [color, setColor] = useState<"red" | "blue">("red");
+  const [color, setColor] = useState<"red" | "blue">("blue");
   const [display, setDisplay] = useState<"grid" | "list">("grid");
   return (
     <div className="app">
-      <div>
-        <input
-          type="checkbox"
-          id="switch"
-          name="theme"
-          onClick={(e) => {
-            setDarkMode(!darkMode);
-          }}
-        />
-        <label htmlFor="switch">Dark Mode</label>
-        <input
-          type="checkbox"
-          id="red"
-          name="theme"
-          onClick={(e) => {
-            setColor(color === "red" ? "blue" : "red");
-          }}
-        />
-        <label htmlFor="blue">red</label>
-      </div>
-      <div>
-        <div style={{ width: "440px", height: "220px" }}>
-          <SwitchView
-            darkMode={darkMode}
-            color={color}
-            display={display}
-            setDisplay={setDisplay}
+      <h1 style={{ textAlign: "center" }}>Switch Challenge</h1>
+      <div
+        style={{
+          backgroundColor: "#21130daa",
+          padding: "2rem",
+          borderRadius: "1rem",
+          marginBottom: "3rem",
+          boxShadow: "0 0 1rem #0e0e0e80",
+        }}
+      >
+        <h2 style={{ textAlign: "center" }}>State Control</h2>
+        <span style={{ marginRight: "1rem" }}>
+          <input
+            type="checkbox"
+            id="switch"
+            name="theme"
+            onClick={(e) => {
+              setDarkMode(!darkMode);
+            }}
           />
-        </div>
+          <label htmlFor="switch">Dark Mode</label>
+        </span>
+        <span>
+          <input
+            type="checkbox"
+            id="color"
+            name="theme"
+            onClick={(e) => {
+              setColor(color === "red" ? "blue" : "red");
+            }}
+          />
+          <label htmlFor="color">Red</label>
+        </span>
+        <p style={{ textAlign: "center" }}>Display Mode: {display}</p>
+      </div>
+
+      <div>
+        <SwitchView
+          darkMode={darkMode}
+          color={color}
+          display={display}
+          setDisplay={setDisplay}
+        />
+        <span
+          style={{
+            fontSize: "1rem",
+            textAlign: "center",
+            width: "100%",
+            display: "block",
+            marginTop: "1rem",
+          }}
+        >
+          Component at 10x
+        </span>
       </div>
     </div>
   );
